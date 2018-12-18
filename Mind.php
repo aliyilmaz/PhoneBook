88ee9762b978f437aa93e34ef6207a8b5ba8420b
Ali YIlmaz <aliyilmaz.work@gmail.com>
2018-12-19 02:59:59 +0300

    The Mind class was updated (Mind v2.2)

--- Mind.php
+++ Mind.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mind;
 
 /**
@@ -1113,39 +1114,93 @@ class Mind {
      * */
     public function mindload($file, $cache=null){
 
-        if(!empty($file) OR !empty($cache)){
+        if (!empty($cache) AND !is_array($cache)) {
+            $cache = array($cache);
+        }
 
-            if (!empty($cache) AND !is_array($cache)) {
-                $cache = array($cache);
-            }
+        if (!empty($cache)) {
+            foreach ($cache as $cachefile) {
 
-            if (!empty($cache)) {
-                foreach ($cache as $cachefile) {
+                $cacheExplode = $this->pGenerator($cachefile);
+                if(!empty($cacheExplode['name'])){
+
+                    $cachefile = $cacheExplode['name'];
+                    $fileName = basename($cacheExplode['name']);
 
                     if (file_exists($cachefile . '.php')) {
                         require_once($cachefile . '.php');
-                    }
-                }
-            }
 
-            if(!empty($file)){
+                        if(class_exists($fileName)){
+                            if(!empty($cacheExplode['params'])){
 
-                $files = array();
+                                $ClassName = new $fileName();
+                                $funcList = get_class_methods($fileName);
 
-                if(!is_array($file)){
-                    $files = array($file);
-                } else {
-                    $files = $file;
-                }
+                                foreach ($cacheExplode['params'] as $param) {
 
-                foreach ($files as $file){
+                                    if(in_array($param, $funcList)){
+                                        $ClassName->$param();
+                                    }
 
-                    if (file_exists($file . '.php')) {
-                        require_once($file . '.php');
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
+
+        if(!empty($file)){
+
+            $files = array();
+
+            if(!is_array($file)){
+                $files = array($file);
+            } else {
+                $files = $file;
+            }
+
+            foreach ($files as $file){
+
+                if (file_exists($file . '.php')) {
+                    require_once($file . '.php');
+                }
+            }
+        }
+    }
+
+    /**
+     * Parameter parser.
+     *
+     * @param string $str
+     * @return mixed|bool
+     *
+     */
+    public function pGenerator($str=null){
+
+        $Result = array();
+        if(!is_null($str)){
+
+            if(strstr($str, ':')){
+                $strExplode = array_filter(explode(':', trim($str, ':')));
+                if(count($strExplode) == 2){
+                    list($filePath, $funcPar) = $strExplode;
+                    $Result['name'] = $filePath;
+
+                    if(strstr($funcPar, '@')){
+                        $funcExplode = array_filter(explode('@', trim($funcPar, '@')));
+                    } else {
+                        $funcExplode = array($funcPar);
+                    }
+                    if(!empty($funcExplode)){
+                        $Result['params'] = $funcExplode;
+                    }
+                }
+            } else {
+                $Result['name'] = $str;
+            }
+        }
+        return $Result;
     }
 
     /**
@@ -1357,23 +1412,12 @@ class Mind {
 
         if(!empty($uri)){
 
-            if(strstr($uri, ':')){
-
-                $tfields = array_filter(explode(':', $uri));
-
-                if(count($tfields)==1){
-                    $uri = implode('', $tfields);
-                }
+            $uriData = $this->pGenerator($uri);
+            if(!empty($uriData['name'])){
+                $uri = $uriData['name'];
             }
-
-            if(!empty($tfields) AND count($tfields)==2){
-
-                list($uri, $tfields) = $tfields;
-                if(strstr($tfields, '@')){
-                    $fields = array_filter(explode('@', $tfields));
-                } else {
-                    $fields = array($tfields);
-                }
+            if(!empty($uriData['params'])){
+                $fields = $uriData['params'];
             }
         }
 
@@ -1499,6 +1543,7 @@ class Mind {
     public function get_contents($left, $right, $url){
 
         set_time_limit(0);
+
         $result = array();
 
         if($this->is_url($url)) {
@@ -1527,8 +1572,6 @@ class Mind {
             }
         }
     }
-
-
 
 }
 ?>
