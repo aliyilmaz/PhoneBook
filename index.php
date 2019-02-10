ab6bb050117b249b9a72be4aa411c81e72e2472e
Ali Yılmaz <aliyilmaz.work@gmail.com>
2019-02-11 01:02:15 +0300

    Mind upgraded. (2.3.3)
    Views directory name updated.
    Error page added.

--- index.php
+++ index.php
@@ -6,32 +6,32 @@ use Mind\Mind;
 $db = new Mind();
 
 $list = array(
-    'app/view/layout/header',
-    'app/view/list',
-    'app/view/layout/footer'
+    'app/views/layout/header',
+    'app/views/list',
+    'app/views/layout/footer'
 );
 $db->route('/', $list , array('app/database/install', 'app/modal/ListModal'));
 
 $db->route('list', $list, 'app/modal/ListModal');
 
 $add = array(
-    'app/view/layout/header',
-    'app/view/add',
-    'app/view/layout/footer'
+    'app/views/layout/header',
+    'app/views/add',
+    'app/views/layout/footer'
 );
 $db->route('add', $add, array('app/controller/RecordController:add'));
 
 $edit = array(
-    'app/view/layout/header',
-    'app/view/edit',
-    'app/view/layout/footer'
+    'app/views/layout/header',
+    'app/views/edit',
+    'app/views/layout/footer'
 );
 $db->route('edit:id', $edit, array('app/modal/RecordModal', 'app/controller/RecordController:edit'));
 
 $remove = array(
-    'app/view/layout/header',
-    'app/view/remove',
-    'app/view/layout/footer'
+    'app/views/layout/header',
+    'app/views/remove',
+    'app/views/layout/footer'
 );
 $db->route('remove:id', $remove, array('app/modal/RecordModal', 'app/controller/RecordController:remove'));
 
