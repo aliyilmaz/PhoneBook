a63e371142c8def3dcc4847373d1d9f5f2acd962
aliyilmaz <aliyilmaz.work@gmail.com>
2019-04-26 15:58:53 +0300

    Mind 2.3.5 sürümüne yükseltilmiştir.

--- app/controller/RecordController.php
+++ app/controller/RecordController.php
@@ -1,67 +1,60 @@
 <?php
 
-class RecordController extends \Mind\Mind
+class RecordController extends Mind
 {
     public $TableName = 'phonebook';
 
     public function __construct($conf = array())
     {
-
         parent::__construct($conf);
     }
 
     public function add()
     {
-
         if (
-            !empty($this->post['name']) AND
-            $this->is_phone($this->post['phone']) AND
+            !empty($this->post['name']) and
+            $this->is_phone($this->post['phone']) and
             $this->is_email($this->post['email'])
-        ){
+        ) {
             $arr = array(
-                'name'=>$this->post['name'],
-                'phone'=>$this->post['phone'],
-                'email'=>$this->post['email'],
-                'created_at'=>date('d-m-Y H:i:s')
+                'name' => $this->post['name'],
+                'phone' => $this->post['phone'],
+                'email' => $this->post['email'],
+                'created_at' => date('d-m-Y H:i:s'),
             );
             $this->insert($this->TableName, $arr);
             $this->redirect();
         }
     }
 
-    public function edit(){
-
+    public function edit()
+    {
         if (
-            $this->do_have($this->TableName, $this->post['id'], 'id') AND
-            !empty($this->post['name']) AND
-            $this->is_phone($this->post['phone']) AND
+            $this->do_have($this->TableName, $this->post['id'], 'id') and
+            !empty($this->post['name']) and
+            $this->is_phone($this->post['phone']) and
             $this->is_email($this->post['email'])
-        ){
+        ) {
             $arr = array(
-                'name'=>$this->post['name'],
-                'phone'=>$this->post['phone'],
-                'email'=>$this->post['email'],
-                'updated_at'=>date('d-m-Y H:i:s')
+                'name' => $this->post['name'],
+                'phone' => $this->post['phone'],
+                'email' => $this->post['email'],
+                'updated_at' => date('d-m-Y H:i:s'),
             );
             $this->update($this->TableName, $arr, $this->post['id']);
             $this->redirect();
-
         }
     }
 
     public function remove()
     {
-
         if (
         $this->do_have($this->TableName, $this->post['id'], 'id')
-        ){
-            if($this->post['status']==true){
+        ) {
+            if ($this->post['status'] == true) {
                 $this->delete($this->TableName, $this->post['id']);
                 $this->redirect();
             }
-
         }
-
     }
-
 }
--- index.php
+++ index.php
@@ -1,38 +1,34 @@
 <?php
 
-require_once ('./Mind.php');
-use Mind\Mind;
-
-$db = new Mind();
+require_once './Mind.php';
+$Mind = new Mind();
 
 $list = array(
     'app/views/layout/header',
     'app/views/list',
-    'app/views/layout/footer'
+    'app/views/layout/footer',
 );
-$db->route('/', $list , array('app/database/install', 'app/modal/ListModal'));
+$Mind->route('/', $list, array('app/database/install', 'app/modal/ListModal'));
 
-$db->route('list', $list, 'app/modal/ListModal');
+$Mind->route('list', $list, 'app/modal/ListModal');
 
 $add = array(
     'app/views/layout/header',
     'app/views/add',
-    'app/views/layout/footer'
+    'app/views/layout/footer',
 );
-$db->route('add', $add, array('app/controller/RecordController:add'));
+$Mind->route('add', $add, array('app/controller/RecordController:add'));
 
 $edit = array(
     'app/views/layout/header',
     'app/views/edit',
-    'app/views/layout/footer'
+    'app/views/layout/footer',
 );
-$db->route('edit:id', $edit, array('app/modal/RecordModal', 'app/controller/RecordController:edit'));
+$Mind->route('edit:id', $edit, array('app/modal/RecordModal', 'app/controller/RecordController:edit'));
 
 $remove = array(
     'app/views/layout/header',
     'app/views/remove',
-    'app/views/layout/footer'
+    'app/views/layout/footer',
 );
-$db->route('remove:id', $remove, array('app/modal/RecordModal', 'app/controller/RecordController:remove'));
-
-?>
+$Mind->route('remove:id', $remove, array('app/modal/RecordModal', 'app/controller/RecordController:remove'));
