d9987cee408dbbbe5e243b579a216b5c82208b7f
aliyilmaz <aliyilmaz.work@gmail.com>
2019-07-03 15:34:24 +0300

    Değişken isimleri güncellendi.

--- app/controller/RecordController.php
+++ app/controller/RecordController.php
@@ -16,13 +16,13 @@ class RecordController extends Mind
             $this->is_phone($this->post['phone']) and
             $this->is_email($this->post['email'])
         ) {
-            $arr = array(
+            $options = array(
                 'name' => $this->post['name'],
                 'phone' => $this->post['phone'],
                 'email' => $this->post['email'],
                 'created_at' => $this->timestamp,
             );
-            $this->insert($this->TableName, $arr);
+            $this->insert($this->TableName, $options);
             $this->redirect();
         }
     }
@@ -35,13 +35,13 @@ class RecordController extends Mind
             $this->is_phone($this->post['phone']) and
             $this->is_email($this->post['email'])
         ) {
-            $arr = array(
+            $options = array(
                 'name' => $this->post['name'],
                 'phone' => $this->post['phone'],
                 'email' => $this->post['email'],
                 'updated_at' => $this->timestamp,
             );
-            $this->update($this->TableName, $arr, $this->post['id']);
+            $this->update($this->TableName, $options, $this->post['id']);
             $this->redirect();
         }
     }
