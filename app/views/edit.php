d826a2db57883186ab1c8b46c8a9451d77449995
aliyilmaz <aliyilmaz.work@gmail.com>
2019-07-03 19:07:02 +0300

    Mind 3.0.0 sürümüne yükseltildi.

--- app/views/edit.php
+++ app/views/edit.php
@@ -4,20 +4,20 @@
 
     <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" name="name" class="form-control" id="name" value="<?=$record['name'];?>">
+        <input type="text" name="name" class="form-control" id="name" value="<?=$record[0]['name'];?>">
     </div>
 
     <div class="form-group">
         <label for="phone">Phone</label>
-        <input type="tel" name="phone" class="form-control" id="phone" value="<?=$record['phone'];?>">
+        <input type="tel" name="phone" class="form-control" id="phone" value="<?=$record[0]['phone'];?>">
     </div>
 
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" name="email" class="form-control" id="email" value="<?=$record['email'];?>">
+        <input type="email" name="email" class="form-control" id="email" value="<?=$record[0]['email'];?>">
     </div>
 
-    <input type="hidden" name="id" value="<?=$record['id'];?>">
+    <input type="hidden" name="id" value="<?=$record[0]['id'];?>">
 
     <button type="submit" class="btn btn-default btn-xs btn-success">
         <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Update
